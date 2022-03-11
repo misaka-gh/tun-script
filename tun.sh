33ce3c9df12e224227ffafdfee42234fcaf3d195
misakano7545 <misakano7545@protonmail.com>
2022-03-12 07:16:42 +0800

    UP

---
+++ tun.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+# 控制台字体
+red() {
+    echo -e "\033[31m\033[01m$1\033[0m"
+}
+
+green() {
+    echo -e "\033[32m\033[01m$1\033[0m"
+}
+
+yellow() {
+    echo -e "\033[33m\033[01m$1\033[0m"
+}
+
+# 判断系统及定义系统安装依赖方式
+REGEX=("debian" "ubuntu" "centos|red hat|kernel|oracle linux|alma|rocky" "'amazon linux'")
+RELEASE=("Debian" "Ubuntu" "CentOS" "CentOS" "Alpine")
+PACKAGE_UPDATE=("apt -y update" "apt -y update" "yum -y update" "yum -y update")
+PACKAGE_INSTALL=("apt -y install" "apt -y install" "yum -y install" "yum -y install" "apk add -f")
+PACKAGE_REMOVE=("apt -y remove" "apt -y remove" "yum -y remove" "yum -y remove")
+
+# 判断是否为root用户
+[[ $EUID -ne 0 ]] && yellow "请在root用户下运行脚本" && exit 1
+
+# 检测系统，本部分代码感谢fscarmen的指导
+CMD=("$(grep -i pretty_name /etc/os-release 2>/dev/null | cut -d \" -f2)" "$(hostnamectl 2>/dev/null | grep -i system | cut -d : -f2)" "$(lsb_release -sd 2>/dev/null)" "$(grep -i description /etc/lsb-release 2>/dev/null | cut -d \" -f2)" "$(grep . /etc/redhat-release 2>/dev/null)" "$(grep . /etc/issue 2>/dev/null | cut -d \\ -f1 | sed '/^[ ]*$/d')")
+
+for i in "${CMD[@]}"; do
+    SYS="$i" && [[ -n $SYS ]] && break
+done
+
+for ((int = 0; int < ${#REGEX[@]}; int++)); do
+    [[ $(echo "$SYS" | tr '[:upper:]' '[:lower:]') =~ ${REGEX[int]} ]] && SYSTEM="${RELEASE[int]}" && [[ -n $SYSTEM ]] && break
+done
+
+[[ -z $SYSTEM ]] && red "不支持VPS的当前系统，请使用主流的操作系统" && exit 1
+
+lxcovz(){
+    case "$(systemd-detect-virt)" in
+        openvz|lxc) echo "" ;;
+        * ) red "不支持的VPS架构！" ;;
+    esac
+}
+
+checkTUN(){
+    TUNStatus=1
+    TUN=$(cat /dev/net/tun 2>&1 | tr '[:upper:]' '[:lower:]')
+    [[ ! $TUN =~ 'in bad state' ]] && [[ ! $TUN =~ '处于错误状态' ]] && [[ ! $TUN =~ 'Die Dateizugriffsnummer ist in schlechter Verfassung' ]] && TUNStatus=0
+}
+
+openTUN(){
+    checkTUN
+    if [ $TUNStatus == 1 ]; then
+        red "检测到VPS已经启用TUN，无需再次启用"
+    fi
+    if [ $TUNStatus == 0 ]; then
+        cd /dev
+        mkdir net
+        mknod net/tun c 10 200
+        chmod 0666 net/tun
+        cat <<EOF >/etc/init.d/tun-statup.sh
+cd /dev
+mkdir net
+mknod net/tun c 10 200
+chmod 0666 net/tun
+EOF
+        checkTUN
+        if [ $TUNStatus == 1 ]; then
+            green "LXC/OVZ VPS的TUN模块已启用成功！"
+        fi
+    fi
+}
+
+lxcovz
+openTUN
