feb4477562f0e6ebbda89386a92960e5ee8b6a5b
Misaka No <96560028+misakano7545@users.noreply.github.com>
2022-05-13 07:07:54 +0800

    Update tun.sh

--- tun.sh
+++ tun.sh
@@ -21,7 +21,7 @@ PACKAGE_INSTALL=("apt -y install" "apt -y install" "yum -y install" "yum -y inst
 PACKAGE_REMOVE=("apt -y remove" "apt -y remove" "yum -y remove" "yum -y remove")
 
 # 判断是否为root用户
-[[ $EUID -ne 0 ]] && yellow "请在root用户下运行脚本" && exit 1
+[[ $EUID -ne 0 ]] && red "请在root用户下运行脚本" && exit 1
 
 # 检测系统，本部分代码感谢fscarmen的指导
 CMD=("$(grep -i pretty_name /etc/os-release 2>/dev/null | cut -d \" -f2)" "$(hostnamectl 2>/dev/null | grep -i system | cut -d : -f2)" "$(lsb_release -sd 2>/dev/null)" "$(grep -i description /etc/lsb-release 2>/dev/null | cut -d \" -f2)" "$(grep . /etc/redhat-release 2>/dev/null)" "$(grep . /etc/issue 2>/dev/null | cut -d \\ -f1 | sed '/^[ ]*$/d')")
